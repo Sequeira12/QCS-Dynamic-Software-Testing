52755d496842e9c1d6f88bf3cb8e51af38a4d3af
Sequeira12 <brunomsequeira@hotmail.com>
2024-05-08 15:54:09 +0100

    quasee

--- Code/Dijkstra.java
+++ Code/Dijkstra.java
@@ -32,7 +32,7 @@ class Graph {
             adjacencyList.put(i, new ArrayList<>());
         }
     }
-
+    
     public void addEdge(int source, int destination, int weight) {
         List<Edge> edges = adjacencyList.get(source);
         edges.add(new Edge(source, destination, weight));
@@ -47,79 +47,57 @@ public class Dijkstra {
     public static void main(String[] args) {
         int numVertices = 5;
         Graph graph = new Graph(numVertices);
-        graph.addEdge(0,2,1);
-        graph.addEdge(0,1,10);
-        graph.addEdge(2,4,5);
-        graph.addEdge(2,3,10);
-        graph.addEdge(3,4,3);
-        int[][] distance = dijkstra(graph, 0);
+        graph.addEdge(0, 1, 10);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(1, 2, 2);
+        graph.addEdge(1, 3, 1);
+        graph.addEdge(2, 1, 3);
+        graph.addEdge(2, 3, 9);
+        graph.addEdge(2, 4, 2);
+        graph.addEdge(3, 4, 4);
+        graph.addEdge(4, 0, 7);
+
+        
+        int[][] distance =  dijkstra(graph, 0);
         for(int i = 0; i < distance.length; i++){
             System.out.printf("%d - %d\n",distance[i][0],distance[i][1]);
         }
     }
-    public static int count = 0;
+
     public static int[][] dijkstra(Graph graph, int source) {
-        if(count == 0)System.out.println("State 1");
+        
         int numVertices = graph.adjacencyList.size();
         boolean[] visited = new boolean[numVertices];
-        
         PriorityQueue<Vertex> priorityQueue = new PriorityQueue<>(numVertices, Comparator.comparingInt(v -> v.distance));
         Vertex[] vertices = new Vertex[numVertices];
-        if(count == 0)System.out.println("State 2");
-        if(count == 0)System.out.println("State 3");
+
         for (int i = 0; i < numVertices; i++) {
-            
             vertices[i] = new Vertex(i);
-            if(count == 0)System.out.println("State 4");
-            if(count == 0)System.out.println("State 6");
             if (i == source) {
-                if(count == 0)System.out.println("State 7");
                 vertices[i].distance = 0;
             }
-            if(count == 0)System.out.println("State 8");
             priorityQueue.add(vertices[i]);
-            if(count == 0)System.out.println("State 3");
-            count++;
-
         }
-        count = 0;
-        if(count == 0)System.out.println("State 5");
+
         while (!priorityQueue.isEmpty()) {
-            if(count == 0)System.out.println("State 9");
             Vertex currentVertex = priorityQueue.poll();
             visited[currentVertex.id] = true;
 
             List<Edge> edges = graph.getEdges(currentVertex.id);
-            if(count == 0)System.out.println("State 10");
             for (Edge edge : edges) {
-                if(count == 0)System.out.println("State 13");
-               // if(count == 0)System.out.printf("%d %d\n",edge.source,edge.weight);
                 if (!visited[edge.destination]) {
-                    if(count == 0)System.out.println("State 14");
                     int newDistance = currentVertex.distance + edge.weight;
-                    if(count == 0)System.out.println("State 15");
-                    System.out.printf("%d   %d\n", newDistance, vertices[edge.destination].distance);
                     if (newDistance < vertices[edge.destination].distance) {
-                        if(count == 0)System.out.println("State 16");
                         priorityQueue.remove(vertices[edge.destination]);
                         vertices[edge.destination].distance = newDistance;
                         priorityQueue.add(vertices[edge.destination]);
                     }
-
                 }
-                
-
-                if(count == 0)System.out.println("State 10");
-                
-                count++;
             }
-            if(count <= 1)System.out.println("State 5");
-            
         }
-        System.out.println("State 5");
-        System.out.println("State 11");
+    
         int[][] distance = distancesV(vertices);
-        System.out.println("State END");
+       
         
         return distance;
     }
--- Code/code.java
+++ Code/code.java
@@ -1,80 +1,50 @@
 package Code;
 
 public class code {
-    static char[][] board = {
-        {'.', '3', '4', '5', '7', '8', '9', '1', '2'},
-        {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
-        {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
-        {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
-        {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
-        {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
-        {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
-        {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
-        {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
-    };
-    public static void solveSudoku(char[][] board) {
-        solve(board,0);
-    }
-    public static boolean solve(char[][]board,int level){
-        if(level==0) System.out.print("  2");
-        if(level==0) System.out.print("  3");
-        for(int i=0;i<9;i++){
-            if(level==0 && i>0) System.out.print("  3");
-            
-            if(level==0) System.out.print( "  4");
-            if(level==0) System.out.print( "  5");
-            for(int j=0;j<9;j++){
-                if(level==0 && j>0) System.out.print( "  5");
-                if(level==0) System.out.print( "  7");
-                if(board[i][j]=='.'){
-                    if(level==0) System.out.print( "  9");
-                        
-                    if(level==0) System.out.print( "  10");
-                        
-                    for(char num='1';num<='9';num++){
-                        if(level==0 && num!='1') System.out.print( "  10");
-                        
+   //###########################################################################
+   static char[][] board = {
+    {'.', '3', '4', '5', '7', '8', '9', '1', '2'},
+    {'6', '7', '2', '1', '9', '5', '3', '4', '8'},
+    {'1', '9', '8', '3', '4', '2', '5', '6', '7'},
+    {'8', '5', '9', '7', '6', '1', '4', '2', '3'},
+    {'4', '2', '6', '8', '5', '3', '7', '9', '1'},
+    {'7', '1', '3', '9', '2', '4', '8', '5', '6'},
+    {'9', '6', '1', '5', '3', '7', '2', '8', '4'},
+    {'2', '8', '7', '4', '1', '9', '6', '3', '5'},
+    {'3', '4', '5', '2', '8', '6', '1', '7', '9'}
+};
+public static void solveSudoku(char[][] board) {
+    solve(board);
+}
+public static boolean solve(char[][]board){
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
 
-                        if(isPossible(board,i,j,num)){
-                            if(level==0) System.out.print( "  12");
-                            board[i][j]=num;
-                            
-                            if(level==0) System.out.print( "  13");
-                     
-                            if(solve(board,level+1)){
-                                if(level==0) System.out.print( "  15");
-                     
-                                return true;
-                            }
-                            //if the solve function return false then the prev num must be in wrong pos so then backtrack
-                            //and fill that wibth '.'. 
-                            else{
-                                if(level==0) System.out.print( "  14");
-                     
-                                board[i][j]='.';
-                            }
+            if(board[i][j]=='.'){
+
+                for(char num='1';num<='9';num++){
+                    if(isPossible(board,i,j,num)){
+                        board[i][j]=num;
+                        if(solve(board)){
+                            return true;
                         }
-                     
-                        if(level==0) System.out.print( "  11");
-                     
+                        //if the solve function return false then the prev num must be in wrong pos so then backtrack
+                        //and fill that with '.'. 
+                        else{
+                            board[i][j]='.';
+                        }
                     }
-                    if(level==0) System.out.print( "  8");
-
-                    
-                    //if it is not possible to select a number from 1 to 9 the solve function return false;
-                    return false;
                 }
-                if(level==0) System.out.print( "  6");
-  
-            }
-            if(level==0) System.out.print( "  16");
 
+                //if it is not possible to select a number from 1 to 9 the solve function return false;
+                return false;
+            }
         }
-        if(level==0) System.out.print("  15");
-        
-        
-        return true;
     }
+    return true;
+}
+
+    
 
     public static boolean isPossible(char[][]board,int row,int col,char c){
         for(int i=0;i<9;i++){
@@ -89,9 +59,9 @@ public class code {
     }
 
     public static void main(String[] args){
-        System.out.print( "  1");
+       
         solveSudoku(board);
-        System.out.println();
+       
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 System.out.print( board[i][j] + " ");
